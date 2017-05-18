51b6c411a8191aef612e33c3fa2c6780c9cf5d4a
Jakub Dyszkiewicz <jakub.dyszkiewicz@gmail.com>
2017-05-18 18:53:43 +0200

    Generated project from spring.io
    Generated with:
    - JPA
    - MySQL
    - Lombok
    - Web
    - Validation

---
+++ src/main/java/org/jakubd/moviedb/MoviedbApplication.java
@@ -0,0 +1,12 @@
+package org.jakubd.moviedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviedbApplication.class, args);
+	}
+}
